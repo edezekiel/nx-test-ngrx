086e79b80196caf87163dd962269f2d04f1f6d07
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-07-16 14:06:23 -0500

    Init WidgetsFacade on HomeComponent onInit

--- apps/dashboard/src/app/home/home.component.ts
+++ apps/dashboard/src/app/home/home.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { WidgetsFacade } from '@nx-test-ngrx/widgets/data-access';
 @Component({
   selector: 'nx-test-ngrx-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent  {
+export class HomeComponent implements OnInit {
+  constructor(private readonly _widgetsFacade: WidgetsFacade) {}
+  
+  ngOnInit() {
+    this._widgetsFacade.init();
+  }
 }
