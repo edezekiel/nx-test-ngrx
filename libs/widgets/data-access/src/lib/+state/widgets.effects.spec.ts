d125d58c17b5b64a74477f713ca891078ef8f422
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2022-07-16 14:34:44 -0500

    Add spec for init$ Widgets Effect

--- libs/widgets/data-access/src/lib/+state/widgets.effects.spec.ts
+++ libs/widgets/data-access/src/lib/+state/widgets.effects.spec.ts
@@ -3,8 +3,8 @@ import { provideMockActions } from '@ngrx/effects/testing';
 import { Action } from '@ngrx/store';
 import { provideMockStore } from '@ngrx/store/testing';
 import { NxModule } from '@nrwl/angular';
-import { hot } from 'jasmine-marbles';
-import { Observable } from 'rxjs';
+import { firstValueFrom, Observable, of } from 'rxjs';
+import { WidgetsDataService } from '../services/widgets-data.service';
 
 import * as WidgetsActions from './widgets.actions';
 import { WidgetsEffects } from './widgets.effects';
@@ -12,12 +12,21 @@ import { WidgetsEffects } from './widgets.effects';
 describe('WidgetsEffects', () => {
   let actions: Observable<Action>;
   let effects: WidgetsEffects;
+  let allWidgetsSpy: jest.Mock<any, any>;
 
   beforeEach(() => {
+    allWidgetsSpy = jest.fn();
+
     TestBed.configureTestingModule({
       imports: [NxModule.forRoot()],
       providers: [
         WidgetsEffects,
+        {
+          provide: WidgetsDataService,
+          useValue: {
+            all: allWidgetsSpy
+          }
+        },
         provideMockActions(() => actions),
         provideMockStore(),
       ],
@@ -27,14 +36,18 @@ describe('WidgetsEffects', () => {
   });
 
   describe('init$', () => {
-    it('should work', () => {
-      actions = hot('-a-|', { a: WidgetsActions.initWidgets() });
-
-      const expected = hot('-a-|', {
-        a: WidgetsActions.loadWidgetsSuccess({ widgets: [] }),
-      });
-
-      expect(effects.init$).toBeObservable(expected);
-    });
+    it('should return loadWidgetsSuccess', async () => {
+      // Arrange
+      const widgets = [{id: 1, name: "Widget 01"}];
+      allWidgetsSpy.mockReturnValue(of(widgets));
+      const expected = WidgetsActions.loadWidgetsSuccess({ widgets });
+      // Act
+      actions = of(WidgetsActions.initWidgets());
+      // Await
+      const result = await firstValueFrom(effects.init$);
+      // Assert
+      expect(result).toEqual(expected);
+      expect(allWidgetsSpy).toBeCalled();
+    })
   });
 });
