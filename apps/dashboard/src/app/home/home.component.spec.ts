71a39688146b7979422b50d59c5cbf9391e1fa9c
Edward Ezekiel <ed.a.ezekiel@gmail.com>
2023-03-07 20:51:31 -0600

    Implement Injector mock

--- apps/dashboard/src/app/home/home.component.spec.ts
+++ apps/dashboard/src/app/home/home.component.spec.ts
@@ -1,27 +1,46 @@
-import { widgetsFacadeInjector } from '@nx-test-ngrx/widgets/data-access';
+import { Injector } from '@angular/core';
+import { WidgetsFacade } from '@nx-test-ngrx/widgets/data-access';
+import { BehaviorSubject, firstValueFrom } from 'rxjs';
 import { HomeComponent } from './home.component';
 
-jest.mock('@nx-test-ngrx/widgets/data-access');
+// jest.mock('@nx-test-ngrx/widgets/data-access');
 
-(widgetsFacadeInjector as jest.Mock).mockImplementation(() => ({
-  init: jest.fn(),
-}));
+// (widgetsFacadeInjector as jest.Mock).mockImplementation(() => ({
+//   init: jest.fn(),
+// }));
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
+  let facadeMock: Partial<WidgetsFacade>;
+  let injector: Injector;
 
   beforeEach(() => {
-    component = new HomeComponent();
+    facadeMock = {
+      init: jest.fn(),
+      loaded$: new BehaviorSubject<boolean>(false),
+    };
+    injector = Injector.create({
+      providers: [
+        { provide: WidgetsFacade, useValue: facadeMock },
+        { provide: HomeComponent, deps: [WidgetsFacade] },
+      ],
+    });
+    component = injector.get(HomeComponent);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
   it('ngOnInit() should invoke facade.init', () => {
     // Act
     component.ngOnInit();
     // Assert
     expect(component['_widgetsFacade'].init).toBeCalled();
+  });
+  it('loaded$ should default to false', async () => {
+    // Await
+    const result = await firstValueFrom(component.loaded$);
+    // Assert
+    expect(result).toBe(false);
   });
 });
